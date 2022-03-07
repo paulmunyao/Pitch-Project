c3cb894dbe0f7ff2610923155aaf54cae57060e5
Paul Munyao <paulmunyao094@gmail.com>
2022-03-07 07:41:34 +0300

    Function for user regiastration added

--- app/routes.py
+++ app/routes.py
@@ -52,3 +52,18 @@ def index():
     user = {"username": "WELCOME TO THE DEN"}
     posts = {"Welcome to the den where powerful ideas are shared and if you're not prepared one can be eaten or as they say the hunter becomes the hunted"}
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
