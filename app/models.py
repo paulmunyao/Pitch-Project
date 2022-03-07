27de8ec9fe0dc72a8b58ae734ab213b243e727a2
Paul Munyao <paulmunyao094@gmail.com>
2022-03-07 15:13:36 +0300

    Add class to get avatar of the user

--- app/models.py
+++ app/models.py
@@ -1,15 +1,22 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
-class User(UserMixin,db.Model):
+
+class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))       
+    return User.query.get(int(id))
 
+
+class User(UserMixin, db.model):
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
