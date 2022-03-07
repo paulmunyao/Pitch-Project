ab951533ffdb5ee3e57078f2c338d25b3b190073
Paul Munyao <paulmunyao094@gmail.com>
2022-03-07 05:10:29 +0300

    Routing Login forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -9,4 +10,7 @@ def index():
     posts = {"Welcome to the den where powerful ideas are shared and if you're not prepared one can be eaten or as they say the hunter becomes the hunted"}
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-   
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)   
