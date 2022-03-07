38b423400bddc715e6452dde2e3f6a627c8d48d2
Paul Munyao <paulmunyao094@gmail.com>
2022-03-07 03:48:49 +0300

    Defining error path for error 404

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,7 @@ def index():
     user = {"username": "WELCOME TO THE DEN"}
     posts = {"Welcome to the den where powerful ideas are shared and if you're not prepared one can be eaten or as they say the hunter becomes the hunted"}
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
