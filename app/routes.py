a9d15b8cb6ae427dac92b2af7dc615611ef0cb05
Paul Munyao <paulmunyao094@gmail.com>
2022-03-07 03:43:00 +0300

    Add introdctory for the application

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,5 @@ from app import app
 @app.route('/index')
 def index():
     user = {"username": "WELCOME TO THE DEN"}
-    return render_template('index.html', title='Home', user=user)
+    posts = {"Welcome to the den where powerful ideas are shared and if you're not prepared one can be eaten or as they say the hunter becomes the hunted"}
+    return render_template('index.html', title='Home', user=user, posts=posts)
