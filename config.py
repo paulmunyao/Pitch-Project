d9ae19d9b6b85b4c7fccfe2f39276f8273a0dd23
Paul Munyao <paulmunyao094@gmail.com>
2022-03-09 09:29:47 +0300

    Modification set to false incase of any change in the app

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = 'guess-me'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
