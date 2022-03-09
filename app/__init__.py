db049bfe721239c986162442c4fd058bc7d5e844
Paul Munyao <paulmunyao094@gmail.com>
2022-03-09 06:52:16 +0300

    Import flask sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from config import Config
 from flask_login import LoginManager
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(Config)
 login = LoginManager(app)
