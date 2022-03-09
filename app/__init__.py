c3c5476d56e40cd1df2518b9b4dd71d6d3c5b01a
Paul Munyao <paulmunyao094@gmail.com>
2022-03-09 14:17:25 +0300

    Reposition import of errors

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,10 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 from app import routes, models, errors
+
