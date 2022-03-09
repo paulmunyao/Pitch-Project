fb01ecae73c820cfabffdeb070077659bfcb8e30
Paul Munyao <paulmunyao094@gmail.com>
2022-03-09 16:16:27 +0300

    Modify files

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+# from sqlalchemy.orm import relationships
 
 
 app = Flask(__name__)
@@ -12,5 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
 from app import routes, models, errors
 
--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from hashlib import md5
 from app import db
 
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
