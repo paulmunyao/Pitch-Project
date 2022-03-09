01f547216e63695962b824d8bbfc001c07aa47ba
Paul Munyao <paulmunyao094@gmail.com>
2022-03-09 12:09:20 +0300

    Set falsk env variable to dvelopment

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    # FLASK_ENV='development'
+    FLASK_ENV='development'
     SECRET_KEY = 'guess-me'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
