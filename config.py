1547569e61f603827825e9438c82807f43145eff
Paul Munyao <paulmunyao094@gmail.com>
2022-03-09 12:10:23 +0300

    Set configurations for database

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     FLASK_ENV='development'
     SECRET_KEY = 'guess-me'
+
+    #Database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
