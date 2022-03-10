5741129d421aeeb9e368da019631c7f34828beb4
Paul Munyao <paulmunyao094@gmail.com>
2022-03-10 03:13:50 +0300

    Deployment

--- app/models.py
+++ app/models.py
@@ -5,23 +5,20 @@ from hashlib import md5
 from app import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    # posts = db.relationships('Post',backref='author',lazy='random')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    # def set_password(self, password):
+    #     self.password_hash = generate_password_hash(password)
 
-class User(UserMixin, db.Model):
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    # def check_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
 
 
 @login.user_loader
