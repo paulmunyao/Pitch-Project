9637689744529004f4a9ad1d3e3795e559ac8ec3
Paul Munyao <paulmunyao094@gmail.com>
2022-03-12 04:54:35 +0300

    Modify files

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
