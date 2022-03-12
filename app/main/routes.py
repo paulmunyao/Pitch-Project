5779656d5db3e06fd4efe4ea9889aa5624f6ee9d
Paul Munyao <paulmunyao094@gmail.com>
2022-03-13 01:02:48 +0300

    Modify files

--- app/main/routes.py
+++ app/main/routes.py
@@ -23,7 +23,7 @@ def Login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.password_hash==form.password.data:
             flash('Invalid username or password')
-            return redirect(url_for('Login'))
+            return redirect(url_for('main.Login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
@@ -41,11 +41,11 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data,password_hash=form.password.data)
-        user.set_password(form.password.data)
+        # user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('Login'))
+        return redirect(url_for('main.Login'))
     return render_template('register.html', title='Register', form=form)
 
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-# from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
@@ -14,11 +14,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    # def set_password(self, password):
-    #     self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    # def check_password(self, password):
-    #     return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 @login.user_loader
