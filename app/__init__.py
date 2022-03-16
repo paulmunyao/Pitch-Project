821465f736febd2152b8f9d3126dc42373643bfc
Paul Munyao <paulmunyao094@gmail.com>
2022-03-16 21:45:45 +0300

    Add file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import os
+
+from config import Config, config_options
 from flask import Flask
-from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from config import config_options
+
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,16 +13,15 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(Config)
 
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://moringa:Access@localhost/pitch"
 
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     from .models import User
     login_manager.init_app(app)
