dd2226c4af3d4a30901476dae936c41efba7919a
Paul Munyao <paulmunyao094@gmail.com>
2022-03-17 11:10:19 +0300

    Modify input forms

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,8 @@ def create_app(config_name):
 
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 
     db.init_app(app)
     with app.app_context():
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app.models import User, Post
 from app import create_app
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 
